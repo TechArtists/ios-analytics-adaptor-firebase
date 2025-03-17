68925e89da4bfd8a24acfd7e8115a9a3cfa68600
Tataru Robert <robert.tataru98@gmail.com>
2025-03-17 12:50:50 +0200

    Update Swift version

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-// swift-tools-version:5.7
+// swift-tools-version:6.0
 
 import PackageDescription
 
